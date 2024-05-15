1dd4773cfb0095b7b9306cf6e7ac6a81751b5267
Elizabeth Wilson <lizwi@192.168.254.152>
2024-05-15 11:28:07 +0800

    Updating whitenoise settings again

--- django_tailwind_app/settings.py
+++ django_tailwind_app/settings.py
@@ -135,8 +135,8 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-    # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    STATICFILES_STORAGE = 'django_tailwind_app.storage.WhiteNoiseStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    # STATICFILES_STORAGE = 'django_tailwind_app.storage.WhiteNoiseStaticFilesStorage'
 
 WHITENOISE_MANIFEST_STRICT = False
 
